ec1ee440912857757b8b02990d24760bce2d7ed3
zadragon <mase0804@naver.com>
2023-05-01 22:58:20 +0900

    update

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,37 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { Divider, Header, Segment } from "semantic-ui-react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Button, Divider, Header, Segment } from "semantic-ui-react";
 import { todoInit } from "../redux/modules/todos";
 
 const Detail = () => {
-	// const dispatch = useDispatch();
-	// useEffect(() => {
-	// 	const getData = JSON.parse(localStorage.getItem("appTodo"));
-	// 	console.log("getData", getData);
-	// 	if (getData !== null) {
-	// 		dispatch(todoInit(getData));
-	// 	}
-	// }, []);
 	const { id } = useParams();
+	const navigate = useNavigate();
 	const todos = useSelector((state) => state.todos);
-	const article = todos.filter((item) => {
-		return item.id === id;
-	});
-	const { id: todoId, title, date, body } = article[0];
+	const [article, setArticle] = useState(null);
+
+	useEffect(() => {
+		const article = todos.filter((item) => {
+			return item.id === id;
+		});
+		if (article.length !== 0) {
+			setArticle(article[0]);
+		} else {
+			navigate("/");
+		}
+	}, []);
+
+	const movePrev = () => {
+		navigate("/");
+	};
 
 	return (
-		<div>
-			<Segment>
-				<Header as="h2" floated="right">
-					<div style={{ textAlign: "right" }}>{title}</div>
-					<div style={{ fontSize: "11px" }}>{date}</div>
-				</Header>
-				<Divider clearing />
-				ID | {todoId}
-				<div>{body}</div>
-			</Segment>
-		</div>
+		<>
+			{article ? (
+				<div>
+					<Segment>
+						<Header
+							as="h2"
+							floated="right"
+							style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-between" }}
+						>
+							<Button basic color="gray" style={{ height: "50px" }} onClick={movePrev}>
+								⬅ 이전으로
+							</Button>
+
+							<div>
+								<div style={{ textAlign: "right" }}> {article.title}</div>
+								<div style={{ fontSize: "11px" }}>{article.date}</div>
+							</div>
+						</Header>
+						<Divider clearing />
+						ID | {article.id}
+						<div>{article.body}</div>
+					</Segment>
+				</div>
+			) : null}
+		</>
 	);
 };
 
